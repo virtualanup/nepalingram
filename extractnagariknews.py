12d0d16338fcdfde5ff531e946972b5fcd96b7da
Anup pokhrel <virtualanup@gmail.com>
2014-06-08 16:00:18 +0545

    Added extractor for nagarik news

---
+++ extractnagariknews.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from bs4 import BeautifulSoup
+import urllib3
+import os
+
+http = urllib3.PoolManager()
+
+
+def checkDir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+outputdirname = "nagariknewsoutput"
+nagariknewsurl = "http://nagariknews.com/politics/story/"
+# make sure the output directory exist
+checkDir(outputdirname)
+
+# iterate through the news artices.
+for i in range(1343, 20000):
+    filename = os.path.join(outputdirname, str(i))
+
+    # if the output file for the news article already exist, skip it.
+    # this will prevent us from redoing much task if the script gets broken in the
+    # middle of extraction.
+    if os.path.exists(filename):
+        print("Skipping ", i)
+        continue
+    #this is a line and the main purpose of the file is to consider th
+    # try to get the HTML content of the URL
+    articleurl = nagariknewsurl + str(i)
+    print("Extracting content from ", i)
+    r = http.request('GET', articleurl)
+
+    # create a file
+    outputfile = open(filename, 'wb')
+    if r.status == 200:
+        # success. Now try to extract the news portition using beautifulsoup
+        extractor = BeautifulSoup(r.data)
+        # the content inside division with ID 'newsbox' is the main content
+        introtext = extractor.find("div", {"class": "itemIntroText"})
+        fulltext = extractor.find("div", {"class": "itemFullText"})
+        content = bytes(introtext.get_text(), 'UTF-8')
+        content += bytes(fulltext.get_text(), 'UTF-8')
+        outputfile.write(bytes(' ', 'UTF-8').join(content.split(bytes('\n', 'UTF-8'))))
+
+    outputfile.close()
