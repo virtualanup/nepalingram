f09ac4d645e2c6404d4d95397afb031e3211dcb6
Anup pokhrel <virtualanup@gmail.com>
2014-06-23 18:59:23 +0545

    Added exception handler in setopati extractor also

--- extractsetopati.py
+++ extractsetopati.py
@@ -33,17 +33,19 @@ for i in range(2000, 12000):
     # create a file
     outputfile = open(filename, 'wb')
     if r.status == 200:
-        # success. Now try to extract the news portition using beautifulsoup
-        extractor = BeautifulSoup(r.data)
-        # the content inside division with ID 'newsbox' is the main content
-        newsbox = extractor.find("div", {"id": "newsbox"})
-        if len(newsbox) > 1:
-            # if there is news inside the news box, then it's length will be > 1
-            # remove the content inside span, h1, h2 etc
-            for htmltag in ['strong', 'h1','span', 'h2']:
-                for tag in newsbox.find_all(htmltag):
-                    tag.decompose()
-            content = bytes(newsbox.get_text(), 'UTF-8')
-            outputfile.write(bytes(' ', 'UTF-8').join(content.split(bytes('\n', 'UTF-8'))))
-
+        try:
+            # success. Now try to extract the news portition using beautifulsoup
+            extractor = BeautifulSoup(r.data)
+            # the content inside division with ID 'newsbox' is the main content
+            newsbox = extractor.find("div", {"id": "newsbox"})
+            if len(newsbox) > 1:
+                # if there is news inside the news box, then it's length will be > 1
+                # remove the content inside span, h1, h2 etc
+                for htmltag in ['strong', 'h1','span', 'h2']:
+                    for tag in newsbox.find_all(htmltag):
+                        tag.decompose()
+                content = bytes(newsbox.get_text(), 'UTF-8')
+                outputfile.write(bytes(' ', 'UTF-8').join(content.split(bytes('\n', 'UTF-8'))))
+        except:
+            pass # do nohing in case of error
     outputfile.close()
