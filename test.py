1c74fea6d8647e27076f246cca981502dc72616d
Anup pokhrel <virtualanup@gmail.com>
2014-07-01 20:58:56 +0545

    Added test files

---
+++ test.py
@@ -0,0 +1,13 @@
+# test.py
+# test the ngram model creator
+# by anup pokhrel
+# http://virtualanup.com
+
+from makengram import ngrammodel
+import re
+
+model = ngrammodel(1)
+model.readfile(open('test'))
+model.saveoutput(open('testoutput','w+'))
+#Data = 'उल्लेख गरिएको छ । दस्तावेजमा एमाओवादीसँग? कार्यगत एकत! is ther'
+#print(Data.replace("।",' ').split())
