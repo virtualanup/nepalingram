9e66f06d11a9969ec6c78a28bdaf0a8fdd11229b
Anup pokhrel <virtualanup@gmail.com>
2014-07-01 21:32:50 +0545

    First working version of ngram generator

--- makengram.py
+++ makengram.py
@@ -24,12 +24,14 @@ class ngrammodel:
         # some symbols. We will then process the sentence
 
         # endsymbols represent the symbols used to end sentences
-        endsymbols = ['?','!','.',';']
+        endsymbols = ['?','!','.',';','\n']
         for symbol in endsymbols:
             article.replace(symbol,'।') #replace with end of sentence symbol
         sentences = article.split('।')
         for sentence in sentences:
-            self.processsentence(sentence)
+            # sentence must be of enough length
+            if len(sentence) > 10:
+                self.processsentence(sentence)
 
     def processsentence(self,sentence):
         '''
@@ -38,9 +40,26 @@ class ngrammodel:
         endsymbols = ['-',',','\'','"','\t']
         for symbol in endsymbols:
             sentence.replace(symbol,' ')
-        words = sentence.split(' ')
+        if self.n > 1:
+            # record the start and end of sentences by a chinese letter (to make
+            # sure it won't exist in text data
+            words = ['西']+sentence.split(' ')+['西']
+        else:
+            words = sentence.split(' ')
+
+        wordlist = []
         for word in words:
-            self.words[word] += 1
+            # to meet the requirements of being a word, some of the predefined characters
+            # must appear in it
+            validletters=['क','ख',u'ग',u'घ',u'ङ',u'च',u'छ',u'ज',u'झ',u'ञ',u'ट',u'ठ',u'ड',u'ढ',u'ण',u'त',u'थ',u'द',u'ध',u'न',u'प',u'फ',u'ब',u'भ',u'म',u'य',u'र',u'ल',u'व',u'श',u'ष',u'स',u'ह',u'अ',u'आ',u'इ',u'ई',u'उ',u'ऊ',u'ए',u'ऐ',u'ओ',u'औ',u'अ',u'अ',u'०',u'१',u'२',u'३',u'४',u'५',u'६',u'७',u'८',u'९','西']
+            
+            for letter in validletters:
+                if letter in word:
+                    wordlist.append(word)
+                    if(len(wordlist) == self.n):
+                        self.words[' '.join(wordlist)] += 1
+                        wordlist = wordlist[1:]
+                    break # break to next word
         
     def readfile(self,file):
         '''
@@ -54,11 +73,5 @@ class ngrammodel:
         '''
         saves the output in the given file
         '''
-        for word in sorted(self.words, key=self.words.get, reverse=True):
-            validletters=['क','ख',u'ग',u'घ',u'ङ',u'च',u'छ',u'ज',u'झ',u'ञ',u'ट',u'ठ',u'ड',u'ढ',u'ण',u'त',u'थ',u'द',u'ध',u'न',u'प',u'फ',u'ब',u'भ',u'म',u'य',u'र',u'ल',u'व',u'श',u'ष',u'स',u'ह',u'अ',u'आ',u'इ',u'ई',u'उ',u'ऊ',u'ए',u'ऐ',u'ओ',u'औ',u'अ',u'अ',u'०',u'१',u'२',u'३',u'४',u'५',u'६',u'७',u'८',u'९']
-            # at least one of the avove letters must occur at least once in the text
-            # to be considered as a valid word
-            for letter in validletters:
-                if letter in word:
-                    file.write(word+' '+str(self.words[word])+"\n")
-                    break
+        for wordseq in sorted(self.words, key=self.words.get, reverse=True):
+            file.write(wordseq+' '+str(self.words[wordseq])+"\n")
